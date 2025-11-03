d5823b07617f33cd082b97cff7fde360ee093a18
Cristian Omar Jiménez Sánchez <cristianomar71@outlook.com>
2025-11-03 13:08:12 -0600

    Se agregan operaciones crud al realizar una venta

---
+++ src/transactions/transactions.service.ts
@@ -0,0 +1,117 @@
+import { BadRequestException, ConsoleLogger, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction, TransactionContents } from './entities/transaction.entity';
+import { Between, FindManyOptions, Repository } from 'typeorm';
+import { Product } from 'src/products/entities/product.entity';
+import { endOfDay, isValid, parseISO, startOfDay } from 'date-fns';
+
+@Injectable()
+export class TransactionsService {
+
+  constructor(
+    @InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,
+    @InjectRepository(TransactionContents) private readonly transactionContentsRepository: Repository<TransactionContents>,
+    @InjectRepository(Product) private readonly productRepository: Repository<Product>
+  ) { }
+
+  async create(createTransactionDto: CreateTransactionDto) {
+
+    await this.productRepository.manager.transaction( async (transactionEntityManager) => {
+      const transaction = new Transaction()
+      transaction.total = createTransactionDto.contents.reduce( (total, item) =>  total + (item.quantity * item.price), 0 )
+
+      for (const contents of createTransactionDto.contents) {
+        const product = await transactionEntityManager.findOneBy(Product, { id: contents.productId })
+        const errors : string[] = []
+
+        if (!product) {
+          errors.push(`El producto con ID ${contents.productId} no fue encontrado`)
+          throw new NotFoundException(errors)
+        }
+
+        if (contents.quantity > product.inventory) {
+          errors.push(`El artículo ${product.name} excede la cantidad disponible`)
+          throw new BadRequestException(errors)
+        }
+
+        product.inventory -= contents.quantity
+
+        //Create TransactionContents instance
+        const transactionContent = new TransactionContents()
+        transactionContent.price = contents.price
+        transactionContent.product = product
+        transactionContent.quantity = contents.quantity
+        transactionContent.transaction = transaction
+
+        await transactionEntityManager.save(transaction)
+        await transactionEntityManager.save(transactionContent)
+      }
+    })
+
+    return 'Venta Almacenada Correctamente'
+  }
+
+  findAll( transactionDate?: string ) {
+    const options : FindManyOptions<Transaction> = {
+      relations: {
+        contents: true
+      }
+    }
+
+    if(transactionDate){
+      const date = parseISO(transactionDate)
+      if(!isValid(date)){
+        throw new BadRequestException('Fecha no válida')
+      }
+
+      const start = startOfDay(date)
+      const end = endOfDay(date)
+
+      options.where = {
+        transactionDate: Between(start, end)
+      }
+    }
+
+    return this.transactionRepository.find(options)
+  }
+
+  async findOne(id: number) {
+    const transaction = await this.transactionRepository.findOne({
+      where: {
+        id
+      },
+      relations:{
+        contents: true
+      }
+    })
+
+    if(!transaction){
+      throw new NotFoundException('Transacción no encontrada')
+    }
+
+    return transaction
+  }
+
+  update(id: number, updateTransactionDto: UpdateTransactionDto) {
+    return `This action updates a #${id} transaction`;
+  }
+
+  async remove(id: number) {
+    const transaction = await this.findOne(id)
+
+    for(const contents of transaction.contents){
+
+      const product = await this.productRepository.findOneBy({id: contents.product.id})
+      product!.inventory += contents.quantity
+      await this.productRepository.save(product!)
+
+      const transactionContents = await this.transactionContentsRepository.findOneBy({id: contents.id})
+      await this.transactionContentsRepository.remove(transactionContents!)
+    }
+
+    await this.transactionRepository.remove(transaction)
+    return {message : 'Venta Eliminada'}
+  }
+}
